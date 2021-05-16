fcd24f6eb5c9358407f751166afe8ecaa19e298c
Jekzer <84268636+Jekzer@users.noreply.github.com>
2021-05-16 19:15:01 +0200

    added image, reverted html script code

--- kudo-panel.js
+++ kudo-panel.js
@@ -1,36 +1,59 @@
-// // Añade un kudo al issue
-// function addKudo(){  
-//     // Obtenemos el conteo actual, incrementamos y actualizamos
-//     var todo = parseInt($('#kudo-count').val());
-//     todo++;
-//     $('#kudo-count').val(todo);
+// Script que permite añadir kudos a un issue
 
-//     // Obtenemos la 'issue key'
-//     var issueId = getIssueKey();
+function addKudo(){  
+    // Obtenemos el conteo actual de kudos y lo actualizamos
+    var kudos = parseInt($('#kudo-count').val());
+    kudos++;
+    $('#kudo-count').val(kudos);
 
-//     // hacemos la petición a la API rest de JIRA
-//     AP.require(['request'], function(request) {
-//         request({
-//             url: '/rest/api/2/issue/'+issueId+'/properties/kudos',
-//             success: function() {
-//                 refreshTodoPanel();
-//             },
-//             error: function() {
-//                 console.error("Error loading API" + response.status);
-//             },
-//             data: JSON.stringify(todo),
-//             type: "PUT",
-//             contentType: "application/json"
-//         });
-//     });  
-// }
+    // // Obtenemos la 'issue key'
+    // var issueId = getIssueKey();
 
-// // Al clickar botón, incrementamos el contador de Kudos
-// $(function(){
-//     $('#create-button').click(addKudo);
-// });
+    // // hacemos la petición a la API rest de JIRA
+    // AP.require(['request'], function(request) {
+    //     request({
+    //         url: '/rest/api/2/issue/'+issueId+'/properties/kudos',
+    //         success: function() {
+    //             refreshTodoPanel();
+    //         },
+    //         error: function() {
+    //             console.error("Error loading API" + response.status);
+    //         },
+    //         data: JSON.stringify(kudos),
+    //         type: "PUT",
+    //         contentType: "application/json"
+    //     });
+    // });  
+}
 
-// // obtenemos la issuekey que hemos almacenando en un input oculto, a modo de metadato
-// function getIssueKey(){  
-//     return $('#kudo-issue-key').val();
-// }
+// Inicializamos el panel
+$(function(){
+    $('#kudo-button').click(addKudo);
+    //refreshTodoPanel();
+});
+
+function refreshTodoPanel(){  
+    var issueId = getIssueKey();
+    AP.require(['request'], function(request) {
+        request({
+            url: '/rest/api/2/issue/'+issueId+'/properties/kudos',
+            success: function(response) {
+                // Convierte la respuesta a JSON
+                response = JSON.parse(response);
+
+                // Llamamos a una función auxiliar para construir la tabla
+                //buildTodoTable(response.value.todos, "#kudo-count");
+            },
+            error: function() {
+                //buildTodoTable([], "#kudo-count");
+            },
+            contentType: "application/json"
+        });
+    });
+}
+
+
+// obtenemos la issuekey que hemos almacenando en un input oculto, a modo de metadato
+function getIssueKey(){  
+    return $('#kudo-issue-key').val();
+}
