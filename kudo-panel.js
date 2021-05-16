dec4502c65695850bcaad67eece93cbf33bf0880
Jekzer <84268636+Jekzer@users.noreply.github.com>
2021-05-16 16:47:37 +0200

    some changes to adapt to kudos

--- kudo-panel.js
+++ kudo-panel.js
@@ -1,53 +1,23 @@
-// Script que permite añadir kudos a un issue
-
-// Elimina un "todo" de la issue
-function removeTodo(todo){
-
-    var issueId = getIssueKey();
-    var data = getData();
-    var index = data.indexOf(todo);
-    data.splice(index,1)
-    var newData = { todos: data};
-    AP.require(['request'], function(request) {
-        request({
-            url: '/rest/api/2/issue/'+issueId+'/properties/todos',
-            success: function() {
-                refreshTodoPanel();
-            },
-            data: JSON.stringify(newData),
-            type: "PUT",
-            error: function(response) {
-                console.error("Error loading API" + response);
-            },
-            contentType: "application/json"
-        });
-    });
-}
-
 // Crea un 'to do' en la issue
 function createTodo(){  
     // Obtenemos la descripción a partir del input:
     var todo = $('#description-input').val();
-    $('#description-input').val("");
+    $('#description-input').val("Hola");
 
     // Obtenemos la 'issue key'
     var issueId = getIssueKey();
-    // obtenemos los "todo" actuales y metemos uno nuevo
-    var data = getData();
-    var newData = { todos: data }
-    newData.todos.push(todo);
 
     // hacemos la petición a la API rest de JIRA
     AP.require(['request'], function(request) {
         request({
-            url: '/rest/api/2/issue/'+issueId+'/properties/todos',
+            url: '/rest/api/2/issue/'+issueId+'/properties/kudos',
             success: function() {
                 refreshTodoPanel();
             },
             error: function() {
                 console.error("Error loading API" + response.status);
             },
-            data: JSON.stringify(newData),
+            data: JSON.stringify(todo),
             type: "PUT",
             contentType: "application/json"
         });
@@ -65,16 +35,16 @@ function refreshTodoPanel(){
     var issueId = getIssueKey();
     AP.require(['request'], function(request) {
         request({
-            url: '/rest/api/2/issue/'+issueId+'/properties/todos',
+            url: '/rest/api/2/issue/'+issueId+'/properties/kudos',
             success: function(response) {
                 // Convierte la respuesta a JSON
                 response = JSON.parse(response);
 
                 // Llamamos a una función auxiliar para construir la tabla
-                buildTodoTable(response.value.todos, "#todo-list");
+                //buildTodoTable(response.value.todos, "#kudo-count");
             },
             error: function() {
-                buildTodoTable([], "#todo-list");
+                //buildTodoTable([], "#kudo-count");
             },
             contentType: "application/json"
         });
@@ -83,58 +53,58 @@ function refreshTodoPanel(){
 
 // Crea una tabla con los todos dentro del elemento identificado por
 // el selector pasado en el parámetro
-function buildTodoTable(todos, selector) {
+// function buildTodoTable(todos, selector) {
 
-    function buildTableAndReturnTbody(hostElement) {
-        var projTable = hostElement.append('table')
-            .classed({'aui': true});
+//     function buildTableAndReturnTbody(hostElement) {
+//         var projTable = hostElement.append('table')
+//             .classed({'aui': true});
 
-        // table > thead > tr
-        var projHeadRow = projTable.append("thead").append("tr");
-        // añadimos cabeceras a la tabla
-        projHeadRow.append("th").text("Description");
-        projHeadRow.append("th").text("Action");
+//         // table > thead > tr
+//         var projHeadRow = projTable.append("thead").append("tr");
+//         // añadimos cabeceras a la tabla
+//         projHeadRow.append("th").text("Description");
+//         projHeadRow.append("th").text("Action");
 
-        return projTable.append("tbody");
-    }
+//         return projTable.append("tbody");
+//     }
 
-    var rootElement = d3.select(selector);
-    rootElement.html("");
-    var tableBody = buildTableAndReturnTbody(rootElement);
+//     var rootElement = d3.select(selector);
+//     rootElement.html("");
+//     var tableBody = buildTableAndReturnTbody(rootElement);
 
-    if (!todos.length){
-        tableBody
-            .append("div")
-            .append('span')
-            .classed({'aui-lozenge': true, 'aui-lozenge-success': true})
-            .text("No todos for this issue")
-        return;
-    }
+//     if (!todos.length){
+//         tableBody
+//             .append("div")
+//             .append('span')
+//             .classed({'aui-lozenge': true, 'aui-lozenge-success': true})
+//             .text("No todos for this issue")
+//         return;
+//     }
 
-    // por cada uno de los todos:
-    var row = tableBody.selectAll("tr")
-        .data(todos)
-        .enter()
-        .append("tr");
+//     // por cada uno de los todos:
+//     var row = tableBody.selectAll("tr")
+//         .data(todos)
+//         .enter()
+//         .append("tr");
 
-    // Añadimos un td para la descripción
-    row.append("td").append('span')
-        .classed({'aui-label': true})
-        // el contenido del td será el propio todo
-        .text(function(item) { return item; });
+//     // Añadimos un td para la descripción
+//     row.append("td").append('span')
+//         .classed({'aui-label': true})
+//         // el contenido del td será el propio todo
+//         .text(function(item) { return item; });
 
-    // Finalmente, un td con el 'enlace' de elminar
-    row.append("td").append('span')
+//     // Finalmente, un td con el 'enlace' de elminar
+//     row.append("td").append('span')
 
-        .append("a")
+//         .append("a")
 
-        .attr('href', "#")
-        .text("remove")
-        //personalizaremos el evento 'click'
-        .on("click", function (d){
-            removeTodo(d);
-        });
-}
+//         .attr('href', "#")
+//         .text("remove")
+//         //personalizaremos el evento 'click'
+//         .on("click", function (d){
+//             removeTodo(d);
+//         });
+// }
 
 // obtiene los todos cargados en la tabla HTML
 function getData(){  
@@ -143,5 +113,5 @@ function getData(){
 
 // obtenemos la issuekey que hemos almacenando en un input oculto, a modo de metadato
 function getIssueKey(){  
-    return $('#todos-issue-key').val();
+    return $('#kudo-issue-key').val();
 }
